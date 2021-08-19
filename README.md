80e9c3a72e2f2a4d7465c90d34472e3397919737
Alexander Santos <lekxandy@hotmail.com>
2021-08-18 22:17:09 -0300

    Inserido o README.md e CORS

---
+++ README.md
@@ -0,0 +1,4 @@
+# API NODE para controle da assistência técnica
+
+    - Iniciado com o CRUD de cliente
+    - Será implementado o CRUD de Ordem de serviços
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 require('dotenv').config()
 require('./src/database')
@@ -7,6 +8,7 @@ require('./src/database')
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cors())
 app.use(require('./src/routes/Cliente.routes'))
 
 app.listen(PORT, ()=> {
